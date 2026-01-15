16d32c1c050a3e00c6a6ca1395331c0a7e8e3bc6
Камиль Хазиев <k.khaziev@bars.group>
2026-01-15 09:46:53 +0300

    DbContext

---
+++ EJournal/Data/AppDbContext.cs
@@ -0,0 +1,47 @@
+using System.Data.Common;
+using System.Reflection;
+using EJournal.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EJournal.Data;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Semester> Semesters { get; set; }
+    public DbSet<Subject> Subjects { get; set; }
+    public DbSet<LessonDate> LessonDates { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Group>()
+            .HasMany(g => g.Students)
+            .WithOne(s => s.Group)
+            .HasForeignKey(s => s.GroupId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Group>()
+            .HasMany(g => g.LessonDates)
+            .WithOne(ld => ld.Group)
+            .HasForeignKey(ld => ld.GroupId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<LessonDate>()
+            .HasMany(ld => ld.Grades)
+            .WithOne(g => g.LessonDate)
+            .HasForeignKey(g => g.LessonDateId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Student>()
+            .HasMany(s => s.Grades)
+            .WithOne(g => g.Student)
+            .HasForeignKey(g => g.StudentId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
